e091cad5357282fdd02f766eb8a2576d0896a862
AndoRyo <ando.ryo.di@focus-s.com>
2023-03-16 15:57:40 +0900

    isara

--- js/script.js
+++ js/script.js
@@ -13,3 +13,25 @@ $(function(){
        return false;
     });
   });
+
+
+$('.box').hide();
+//アコーディオンをクリックした時の動作
+$('.question, .question2').on('click', function () {//タイトル要素をクリックしたら
+   var findElm = $(this).next(".box");//直後のアコーディオンを行うエリアを取得し
+   $(findElm).slideToggle();//アコーディオンの上下動作
+
+   var findAngle = $(this).children(".ac-angle")
+
+   if ($(this).hasClass('close')) {//タイトル要素にクラス名closeがあれば
+       $(this).removeClass('close');//クラス名を除去し
+       $(this).removeClass('active');
+       $(findAngle).addClass('fa-angle-down');
+       $(findAngle).removeClass('fa-angle-up');
+   } else {//それ以外は
+       $(this).addClass('close');//クラス名closeを付与
+       $(this).addClass('active');
+       $(findAngle).removeClass('fa-angle-down');
+       $(findAngle).addClass('fa-angle-up');
+   }
+});
